680896e1259299be70bbbd1092ad317a0880a97c
Victor Hang <vhvictorhang@gmail.com>
2026-04-25 00:06:46 +0200

    chore 🧹 (yaook): remove SECURE_SSL_REDIRECT from yaook websso config
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- infrastructure/yaook/_10_websso.py
+++ infrastructure/yaook/_10_websso.py
@@ -13,6 +13,7 @@ WEBSSO_KEYSTONE_URL = "https://keystone.rpcu.vpn/v3"
 WEBSSO_INITIAL_CHOICE = "zitadel_openid"
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 USE_X_FORWARDED_HOST = True
+SECURE_SSL_REDIRECT = True
 
 # Use internal endpoint for the Horizon --> Keystone login
 WEBSSO_USE_HTTP_REFERER = False

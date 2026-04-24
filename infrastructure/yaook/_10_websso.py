2ad74faef8ff41a59014af2d37df3bf20687c8ac
Victor Hang <vhvictorhang@gmail.com>
2026-04-24 21:44:34 +0200

    feat ✨ (zitadel): add zitadel oidc integration for openstack sso
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

---
+++ infrastructure/yaook/_10_websso.py
@@ -0,0 +1,16 @@
+from django.utils.translation import gettext_lazy as _
+
+WEBSSO_ENABLED = True
+WEBSSO_CHOICES = (
+    ("credentials", _("Keystone Credentials")),
+    ("zitadel_openid", "Zitadel - SSO"),
+)
+# Identity provider "keycloak" and Federation protocol "openid"
+WEBSSO_IDP_MAPPING = {
+    "zitadel_openid": ("zitadel", "openid"),
+}
+WEBSSO_KEYSTONE_URL = "https://keystone.rpcu.vpn/v3"
+WEBSSO_INITIAL_CHOICE = "zitadel_openid"
+
+# Use internal endpoint for the Horizon --> Keystone login
+WEBSSO_USE_HTTP_REFERER = False
